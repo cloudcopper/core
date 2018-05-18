be4d0500a12dc2d0acf06418519e43139ecc55ff
egorse <egorovhome@gmail.com>
2018-05-18 22:52:29 +0300

    Cosmetic changes

--- encoding/tlv/doc.go
+++ encoding/tlv/doc.go
@@ -2,7 +2,7 @@
 // The mapping between T8L16 and Go values is described in the documentation
 // for the Unmarshal function.
 // It handles only T8L16 input data where Type is 1 octet (byte)
-// and Length is 2 octets (uint16) (i.e. R-PHY RCP).
+// and Length is 2 octets (uint16) (i.e. R-PHY Control Protocol(RCP)).
 //
 // It does not support others formats (i.e. T8L8 as in DOCSIS MULPI).
 // Marshal function is out of scope for this package.
